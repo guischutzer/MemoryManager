510f79bdb17f9ced1b90abd4a00b551a6dc8656a
Guilherme Schützer <guischutzer@gmail.com>
2015-10-06 11:57:56 -0300

    Newline

--- Code/main.c
+++ Code/main.c
@@ -1,3 +1,12 @@
+/*
+
+MAC0422 - Sistemas Operacionais
+    EP2 - 05/10/2015
+
+Guilherme Souto Schützer     - 8658544
+Tomás Marcondes Bezerra Paim - 7157602
+
+*/
 
 #include <stdlib.h>
 #include <string.h>
